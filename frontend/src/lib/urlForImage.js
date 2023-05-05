f903f2ba4770a7a8b7e5c1c10264839631f0c730
Christian Kozalla <christian.kozalla@gmail.com>
2023-05-05 08:28:59 +0200

    feat: add posts to index page + fix workshop images

--- frontend/src/lib/urlForImage.js
+++ frontend/src/lib/urlForImage.js
@@ -3,5 +3,6 @@ import { useSanityClient, createImageBuilder } from 'astro-sanity';
 export const imageBuilder = createImageBuilder(useSanityClient());
 
 export function urlForImage(source) {
+  console.log("IMAGE SOURCE", source);
   return imageBuilder.image(source);
 }
