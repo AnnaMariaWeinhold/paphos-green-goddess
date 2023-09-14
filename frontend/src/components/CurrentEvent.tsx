013a77cb3ed271d504b819e0f74c1e1ad2cfca74
Christian Kozalla <christian.kozalla@gmail.com>
2023-09-14 23:07:23 +0200

    fix: CurrentEvent Component show nearest event in the future

--- frontend/src/components/CurrentEvent.tsx
+++ frontend/src/components/CurrentEvent.tsx
@@ -10,22 +10,29 @@ export function CurrentEvent({ category, slug }: { category: Categories, slug: s
         (async () => {
             try {
                 const sanityEvents: Event[] = await readOnlySanityClient.fetch(`*[_type == "event" && type == "${category}"]`);
-                setEvents(sanityEvents.filter((event) => typeof event.type === "string"));
+                setEvents(sanityEvents.filter((event) => typeof event.type === "string" && event.href.includes(slug)));
             } catch (e) {
                 console.log("Error fetching events from Sanity", e.message || e);
-                setEvents(undefined);
+                setEvents(null);
             }
         })();
     }, []);
 
-    const event = Array.isArray(events) ? events.find((event) => event.href.includes(slug)) : null;
+    const currentDate = new Date();
 
+    const futureEvents = Array.isArray(events)
+        ? events
+            .map((obj) => ({ ...obj, date: new Date(obj.date) }))
+            .filter((obj) => obj.date > currentDate)
+            .sort((a, b) => a.date - b.date) 
+        : null;
+    const event = Array.isArray(futureEvents) && futureEvents.length > 0 ? futureEvents[0] : Array.isArray(events) ? events[0] : null;
 
     return (
         <Fragment>
             {event ?
-                <Fragment>We meet {new Date(event.date).toLocaleDateString("de-DE")}, <br />{new Date(event.date).toLocaleTimeString("en-US", { hour12: true, hour: "2-digit", minute: "2-digit" }).toLowerCase()}</Fragment>
-                : event === null ? <a style={{
+                <Fragment>We meet {new Date(event.date).toLocaleDateString("de-DE")}, <br />{new Date(event.from).toLocaleTimeString("en-US", { hour12: true, hour: "2-digit", minute: "2-digit" }).toLowerCase()}</Fragment>
+                : events === null ? <a style={{
                     color: "inherit",
                     textDecoration: "underline",
                 }} href="/contact/">
