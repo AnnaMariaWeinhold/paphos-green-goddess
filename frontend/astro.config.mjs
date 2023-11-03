239a017b02f5bf0e2e5b7e7d2a15f89c98cf6e10
Christian Kozalla <christian.kozalla@gmail.com>
2023-11-03 08:53:28 +0100

    add site url

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.paphosgreengoddess.com',
   integrations: [sanity({
     projectId: 'l2awlkjo',
     dataset: 'production',
